e6085c59342f703d298fe845dc4c8a6f273534fd
abdelrahman hamada <abdohamada5100@gmail.com>
2023-09-10 14:43:12 +0300

    adding function that searched for the wins

--- app.js
+++ app.js
@@ -20,4 +20,42 @@ events.textContent = `${role} goes first !`
         e.target.appendChild(goItem);
         e.target.removeEventListener('click',addGo)
         events.textContent = `${role} goes !`
-    }
+        checkWins()
+}
+
+let winRoles = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+]
+function checkWins (){
+    window.circleWins ; 
+    window.crossWins ;
+    const allSquares = document.querySelectorAll('.square');
+    winRoles.forEach(array=>{
+        window.circleWins = array.every((cell)=>{
+            return allSquares[cell].firstChild?.classList.contains('circle')?true:false;
+            // ? here  means may be null (there is no memeber property called firsChild )
+        })
+        if(window.circleWins){
+            reload.textContent = "AGAIN"
+            allSquares.forEach(e=>{
+                e.removeEventListener('click',addGo);
+                events.textContent = 'circle won !'
+                events.classList.add('circle-won')
+            })
+        }
+        window.crossWins = array.every((cell)=>{
+            return allSquares[cell].firstChild?.classList.contains('cross')?true:false;
+            // ? here  means may be null (there is no memeber property called firsChild )
+        })
+        if(window.crossWins){
+            reload.textContent = "AGAIN"
+            allSquares.forEach(e=>{
+                e.removeEventListener('click',addGo);
+                events.textContent = 'cross won ! '
+                events.classList.add('cross-won')
+            })
+        }
+    })
+}
